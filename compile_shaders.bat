99df7857ede8a5f42ec51ed0f0f76d13c983c7fd
lucas-stallknecht <lucas.stallknecht.pro@gmail.com>
2024-11-26 14:02:08 +0100

    Debug only : Compile and copy shaders inside build directory before launch

--- compile_shaders.bat
+++ compile_shaders.bat
@@ -1,3 +1,16 @@
-C:/VulkanSDK/1.3.283.0/Bin/glslc.exe ./shaders/camera_test.comp -o ./shaders/camera_test.comp.spv
-C:/VulkanSDK/1.3.283.0/Bin/glslc.exe ./shaders/uv_draw.comp -o ./shaders/uv_draw.comp.spv
-pause
+@echo off
+setlocal enabledelayedexpansion
+set GLSLC="C:/VulkanSDK/1.3.283.0/Bin/glslc.exe"
+set SHADER_DIR=shaders
+set COMPILED_SHADER_BUILD_DIR=cmake-build-debug\shaders
+
+@REM Create the output directory if it doesn't exist
+if not exist "%COMPILED_SHADER_BUILD_DIR%" (
+    mkdir "%COMPILED_SHADER_BUILD_DIR%"
+)
+
+for %%f in (%SHADER_DIR%\*.comp) do (
+    set "OUTPUT_FILE=%COMPILED_SHADER_BUILD_DIR%\%%~nxf.spv"
+    echo Compiling %%f to !OUTPUT_FILE!
+    %GLSLC% %%f -o !OUTPUT_FILE!
+)
